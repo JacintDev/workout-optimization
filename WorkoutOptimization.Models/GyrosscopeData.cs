38cd5ae362b1089713c97a4e126b5a3f6aaa93be
Jacint69 <kovacs.jacint02@gmail.com>
2025-02-19 15:48:54 +0100

    Gyrosscope properties added.

--- WorkoutOptimization.Models/GyrosscopeData.cs
+++ WorkoutOptimization.Models/GyrosscopeData.cs
@@ -5,6 +5,21 @@ namespace WorkoutOptimization.Models
 {
     public class GyrosscopeData
     {
-     
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int GyroscopeDataId { get; set; }
+        [Required]
+        public int AccelX { get; set; }
+        [Required]
+        public int AccelY { get; set; }
+        [Required]
+        public int AccelZ { get; set; }
+        [Required]
+        public int GyrosX { get; set; }
+        [Required]
+        public int GyrosY { get; set; }
+        [Required]
+        public int GyrosZ { get; set; }
+        public DateTime? Date { get; set; }
     }
 }
