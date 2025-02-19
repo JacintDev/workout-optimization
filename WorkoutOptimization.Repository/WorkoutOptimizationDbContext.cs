bed9618a6e3f1f3adaf3a29450f4a4f6fb113fa2
Jacint69 <kovacs.jacint02@gmail.com>
2025-02-19 16:38:00 +0100

    Project init.

---
+++ WorkoutOptimization.Repository/WorkoutOptimizationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkoutOptimization.Models;
+
+namespace WorkoutOptimization.Repository
+{
+    public class WorkoutOptimizationDbContext 
+    {
+       
+    }
+}
