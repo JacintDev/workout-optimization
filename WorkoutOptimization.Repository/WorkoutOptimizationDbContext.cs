b6148c8a0a7bcc69f2838aeaf390f8599982cca7
Jacint69 <kovacs.jacint02@gmail.com>
2025-02-19 16:38:32 +0100

    GyrosScopeData table created, added dummy datas.

--- WorkoutOptimization.Repository/WorkoutOptimizationDbContext.cs
+++ WorkoutOptimization.Repository/WorkoutOptimizationDbContext.cs
@@ -3,8 +3,29 @@ using WorkoutOptimization.Models;
 
 namespace WorkoutOptimization.Repository
 {
-    public class WorkoutOptimizationDbContext 
+    public class WorkoutOptimizationDbContext : DbContext
     {
-       
+        public DbSet<GyrosscopeData> GyrosScropeData { get; set; }
+
+        public WorkoutOptimizationDbContext(DbContextOptions<WorkoutOptimizationDbContext> opt) : base(opt)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<GyrosscopeData>().HasData(new GyrosscopeData()
+            {
+                AccelX = 1,
+                AccelY = 1,
+                AccelZ = 1,
+                GyrosX = 1,
+                GyrosY = 1,
+                GyrosZ = 1,
+                Date = DateTime.Now,
+                GyroscopeDataId = 1
+            });
+        }
     }
 }
