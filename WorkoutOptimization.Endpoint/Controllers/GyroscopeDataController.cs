d5b49adf8023d2522a739b4b49bd11d0da714190
Jacint69 <kovacs.jacint02@gmail.com>
2025-02-19 16:45:16 +0100

    GyroscopeDataController created.

---
+++ WorkoutOptimization.Endpoint/Controllers/GyroscopeDataController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WorkoutOptimization.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GyroscopeDataController : ControllerBase
+    {
+        // GET: api/<GyroscopeDataController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<GyroscopeDataController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<GyroscopeDataController>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<GyroscopeDataController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<GyroscopeDataController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
