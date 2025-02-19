b58216045ea81d581dbada0fc44ad5dd67754b05
Jacint69 <kovacs.jacint02@gmail.com>
2025-02-19 15:07:23 +0100

    Project init

---
+++ WorkoutOptimization.Models/GyrosscopeData.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorkoutOptimization.Models
+{
+    public class GyrosscopeData
+    {
+     
+    }
+}
