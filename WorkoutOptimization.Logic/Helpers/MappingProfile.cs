0f05f0eaf6e6ae0f4bfcdb22a2df2fff584c2594
Jacint69 <kovacs.jacint02@gmail.com>
2025-03-04 14:19:30 +0100

    DTO MappingProfile class created.

---
+++ WorkoutOptimization.Logic/Helpers/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutOptimization.Models;
+
+namespace WorkoutOptimization.Logic.Helpers
+{
+    public class MappingProfile : Profile
+    {
+
+        public MappingProfile()
+        {
+            CreateMap<GyroscopeData, GyroscopeDataDto>().ReverseMap();
+        }
+
+    }
+}
