00f7276302cdeffbb8772afbb69d7c7bd3ab0d60
Jacint69 <kovacs.jacint02@gmail.com>
2025-03-04 14:19:45 +0100

    GyroscopeDataDto class created.

---
+++ WorkoutOptimization.Models/GyroscopeDataDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorkoutOptimization.Models
+{
+    public class GyroscopeDataDto
+    {
+        
+        public float AccelX { get; set; }
+        
+        public float AccelY { get; set; }
+        
+        public float AccelZ { get; set; }
+        
+        public float GyrosX { get; set; }
+        
+        public float GyrosY { get; set; }
+        
+        public float GyrosZ { get; set; }
+        public DateTime? Date { get; set; }
+    }
+}
