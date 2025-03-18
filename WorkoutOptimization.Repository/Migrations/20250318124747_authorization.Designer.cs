36bb79996e7799ffb563e800b81e41491995d7d3
Jacint69 <kovacs.jacint02@gmail.com>
2025-03-18 14:44:44 +0100

    Mssql migrations

--- WorkoutOptimization.Repository/Migrations/20250318124747_authorization.Designer.cs
+++ WorkoutOptimization.Repository/Migrations/20250318124747_authorization.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WorkoutOptimization.Repository;
 
@@ -11,9 +12,11 @@ using WorkoutOptimization.Repository;
 namespace WorkoutOptimization.Repository.Migrations
 {
     [DbContext(typeof(WorkoutOptimizationDbContext))]
-    partial class WorkoutOptimizationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250318124747_authorization")]
+    partial class authorization
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
