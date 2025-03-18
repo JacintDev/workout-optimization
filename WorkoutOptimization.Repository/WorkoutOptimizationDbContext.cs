93557d859a7fa8c79cf6f63ec13c90728a13d4f7
Jacint69 <kovacs.jacint02@gmail.com>
2025-03-18 13:35:59 +0100

    Added IdentityDbContext to database, and created dummy datas.

--- WorkoutOptimization.Repository/WorkoutOptimizationDbContext.cs
+++ WorkoutOptimization.Repository/WorkoutOptimizationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using WorkoutOptimization.Models;
 
 namespace WorkoutOptimization.Repository
 {
-    public class WorkoutOptimizationDbContext : DbContext
+    public class WorkoutOptimizationDbContext : IdentityDbContext<User>
     {
         public DbSet<GyroscopeData> GyrosScropeData { get; set; }
 
@@ -26,6 +28,19 @@ namespace WorkoutOptimization.Repository
                 Date = DateTime.Now,
                 GyroscopeDataId = 1
             });
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole()
+                {
+                    Id = "1",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                }, new IdentityRole()
+                {
+                    Id = "2",
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+
+            );
         }
     }
 }
