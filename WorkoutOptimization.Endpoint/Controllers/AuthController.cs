570054dffcecf4d910f7886fc2c40b954e599ae9
Jacint69 <kovacs.jacint02@gmail.com>
2025-03-18 14:44:27 +0100

    Added AuthController class.

---
+++ WorkoutOptimization.Endpoint/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorkoutOptimization.Models;
+
+namespace WorkoutOptimization.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var user = new User()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                DateOfBirth = model.DateOfBirth,
+                Weight = model.Weight,
+                Height = model.Height,
+                Level = model.Level
+            };
+        }
+    }
+}
