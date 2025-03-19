2fab567081365afce5dcab3f4cafd58606ec6c29
Jacint69 <kovacs.jacint02@gmail.com>
2025-03-19 15:15:49 +0100

    Added login method.

--- WorkoutOptimization.Endpoint/Controllers/AuthController.cs
+++ WorkoutOptimization.Endpoint/Controllers/AuthController.cs
@@ -2,6 +2,10 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using WorkoutOptimization.Logic;
 using WorkoutOptimization.Models;
 
@@ -38,9 +42,35 @@ namespace WorkoutOptimization.Endpoint.Controllers
             else
             {
                 //return bad request with message json format
-                return BadRequest(new { message = "User creation failed" });
+               throw new Exception("User creation failed.");
             }
 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user= await _userManager.FindByEmailAsync(model.Email);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var claim = new List<Claim> { new Claim(JwtRegisteredClaimNames.Sub, user.Email) };
+                foreach (var role in await _userManager.GetRolesAsync(user))
+                {
+                    claim.Add(new Claim(ClaimTypes.Role, role));
+                }
+                var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("nagyonhosszutitkoskodhelyenagyonhosszutitkoskodhelye"));
+                var token = new JwtSecurityToken(
+                issuer: "http://www.security.org", audience: "http://www.security.org",
+                claims: claim, expires: DateTime.Now.AddMinutes(180),
+                signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
         }
     }
 }
