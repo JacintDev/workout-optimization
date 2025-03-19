00fa94b88416864e6151d9cd3e619fe9c4a25072
Jacint69 <kovacs.jacint02@gmail.com>
2025-03-19 13:22:05 +0100

    User, Registermodel classes added.

---
+++ WorkoutOptimization.Models/RegisterModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace WorkoutOptimization.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Length(2, 20)]
+        public string FirstName { get; set; }
+        [Required]
+        [Length(2, 20)]
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        [NotMapped]
+        public int GetAge
+        {
+            get => DateOfBirth.HasValue ? DateTime.Now.Year - DateOfBirth.Value.Year : 0;
+        }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+
+        public Level Level { get; set; }
+        public Sex Sex { get; set; }
+
+    }
+}
--- WorkoutOptimization.Models/User.cs
+++ WorkoutOptimization.Models/User.cs
@@ -25,24 +25,24 @@ namespace WorkoutOptimization.Models
         
     public class User : IdentityUser
     {
-        [Required]
+        
         [Length(2,20)]
-        public string FirstName { get; set; }
-        [Required]
+        public string? FirstName { get; set; }
+        
         [Length(2, 20)]
-        public string LastName { get; set; }
-        [Required]
+        public string? LastName { get; set; }
+        
         public DateTime? DateOfBirth { get; set; }
         [NotMapped]
         public int GetAge 
         {
             get => DateOfBirth.HasValue ? DateTime.Now.Year - DateOfBirth.Value.Year : 0;
         }
-        public int Weight { get; set; }
-        public int Height { get; set; }
+        public int? Weight { get; set; }
+        public int? Height { get; set; }
 
-        public Level Level { get; set; }
-        public Sex Sex { get; set; }
+        public Level? Level { get; set; }
+        public Sex? Sex { get; set; }
 
 
 
