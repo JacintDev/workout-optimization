a48dd29d7e36a902a7ff4d8aeacc12c75eec1308
Jacint69 <kovacs.jacint02@gmail.com>
2025-03-20 16:38:54 +0100

    Added exercise DTO.

---
+++ WorkoutOptimization.Models/ExerciseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutOptimization.Models
+{
+    public class ExerciseDto
+    {
+        //copy properties from Exercise.cs
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public MuscleGroup MuscleGroup { get; set; }
+        public string? Video { get; set; }
+    }
+}
