c2b0e3b663614f9197c2f0a8b68701aeaf5c71ba
Jacint69 <kovacs.jacint02@gmail.com>
2025-03-20 12:08:29 +0100

    Login method logic, moved to logic layer.

--- WorkoutOptimization.Endpoint/Controllers/AuthController.cs
+++ WorkoutOptimization.Endpoint/Controllers/AuthController.cs
@@ -50,25 +50,9 @@ namespace WorkoutOptimization.Endpoint.Controllers
         [HttpPost]
         public async Task<IActionResult> Login([FromBody] LoginModel model)
         {
-            var user= await _userManager.FindByEmailAsync(model.Email);
-            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
-            {
-                var claim = new List<Claim> { new Claim(JwtRegisteredClaimNames.Sub, user.Email) };
-                foreach (var role in await _userManager.GetRolesAsync(user))
-                {
-                    claim.Add(new Claim(ClaimTypes.Role, role));
-                }
-                var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("nagyonhosszutitkoskodhelyenagyonhosszutitkoskodhelye"));
-                var token = new JwtSecurityToken(
-                issuer: "http://www.security.org", audience: "http://www.security.org",
-                claims: claim, expires: DateTime.Now.AddMinutes(180),
-                signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
-                );
-                return Ok(new
-                {
-                    token = new JwtSecurityTokenHandler().WriteToken(token),
-                    expiration = token.ValidTo
-                });
+            var result = _logic.Login(model);
+            if (result.Result.isAuthenticated) { 
+                return Ok(new {token=result.Result.Token, expiration = result.Result.Expiration});
             }
             return Unauthorized();
         }
--- WorkoutOptimization.Logic/IAuthorizationLogic.cs
+++ WorkoutOptimization.Logic/IAuthorizationLogic.cs
@@ -4,6 +4,7 @@ namespace WorkoutOptimization.Logic
 {
     public interface IAuthorizationLogic
     {
+        Task<(bool isAuthenticated, string Token, DateTime Expiration)> Login(LoginModel model);
         Task<bool> Register(RegisterModel model);
     }
 }
