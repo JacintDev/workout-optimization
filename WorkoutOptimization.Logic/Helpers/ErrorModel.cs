b754100c1800c9e1a21bfdcc2748f9bc3873a61f
Jacint69 <kovacs.jacint02@gmail.com>
2025-03-24 18:11:19 +0100

    Exception handling separated from program.cs to helper class.

---
+++ WorkoutOptimization.Logic/Helpers/ErrorModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkoutOptimization.Logic.Helpers
+{
+    public class ErrorModel
+    {
+        public string Message { get; set; }
+
+        public ErrorModel(string message)
+        {
+            this.Message = message;
+        }
+    }
+}
---
+++ WorkoutOptimization.Logic/Helpers/ExceptionFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutOptimization.Logic.Helpers
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var error=new ErrorModel(context.Exception.Message);
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Result = new JsonResult(error);
+        }
+    }
+}
