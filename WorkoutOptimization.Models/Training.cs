9eecf2fe80d611c3f8c04ae347ab96210f0c5376
Jacint69 <kovacs.jacint02@gmail.com>
2025-03-24 18:09:23 +0100

    Added training model.

---
+++ WorkoutOptimization.Models/Training.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutOptimization.Models
+{
+    public class Training
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TrainingId { get; set; }
+        [Required]
+        public DateTime Start {  get; set; }
+        [Required]
+        public DateTime End { get; set; }
+
+        [ForeignKey(nameof(Exercise))]
+        public int ExerciseId { get; set; }
+        [ForeignKey(nameof(User))]
+        public Guid UserId { get; set; }
+
+        public virtual Exercise Exercise { get; set; }
+        public virtual User User { get; set; }
+
+    }
+}
