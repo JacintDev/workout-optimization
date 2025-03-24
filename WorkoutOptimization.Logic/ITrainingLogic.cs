5aca8e7e5b49b97eed4c0792368614a39343f586
Jacint69 <kovacs.jacint02@gmail.com>
2025-03-24 18:09:03 +0100

    Added Training Logic with interface.

---
+++ WorkoutOptimization.Logic/ITrainingLogic.cs
@@ -0,0 +1,13 @@
+﻿using WorkoutOptimization.Models;
+
+namespace WorkoutOptimization.Logic
+{
+    public interface ITrainingLogic
+    {
+        void Create(TrainingDto entity);
+        void Delete(int id);
+        Training Read(int id);
+        IQueryable<Training> ReadAll();
+        void Update(TrainingDto entity, int id);
+    }
+}
---
+++ WorkoutOptimization.Logic/TrainingLogic.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutOptimization.Models;
+using WorkoutOptimization.Repository;
+
+namespace WorkoutOptimization.Logic
+{
+    public class TrainingLogic : ITrainingLogic
+    {
+        readonly IMapper _mapper;
+        readonly IRepository<Training> _repo;
+
+        public TrainingLogic(IRepository<Training> repo, IMapper mapper)
+        {
+            _mapper = mapper;
+            _repo = repo;
+        }
+
+        public void Create(TrainingDto entity)
+        {
+            _repo.Create(_mapper.Map<Training>(entity));
+        }
+
+        public void Delete(int id)
+        {
+            _repo.Delete(id);
+        }
+
+        public Training Read(int id)
+        {
+            return _repo.Read(id);
+        }
+
+        public IQueryable<Training> ReadAll()
+        {
+            return _repo.ReadAll();
+        }
+
+        public void Update(TrainingDto entity, int id)
+        {
+            var ent = _mapper.Map<Training>(entity);
+            ent.TrainingId = id;
+            _repo.Update(ent);
+        }
+
+
+    }
+}
