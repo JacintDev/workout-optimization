382badb24bd14374e66368b0da66327b06615418
Jacint69 <kovacs.jacint02@gmail.com>
2025-03-26 13:03:26 +0100

    Nav component initialized.

--- WorkoutOptimization.FrontEnd/src/app/app.module.ts
+++ WorkoutOptimization.FrontEnd/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { UnathorizedNavComponent } from './unathorized-nav/unathorized-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    UnathorizedNavComponent
   ],
   imports: [
     BrowserModule,
---
+++ WorkoutOptimization.FrontEnd/src/app/unathorized-nav/unathorized-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unathorized-nav',
+  standalone: false,
+  templateUrl: './unathorized-nav.component.html',
+  styleUrl: './unathorized-nav.component.sass'
+})
+export class UnathorizedNavComponent {
+
+}
