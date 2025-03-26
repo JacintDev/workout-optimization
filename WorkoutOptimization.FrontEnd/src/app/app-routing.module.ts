8234efc82b3596b532fc18060b1cdc42140d19bd
Jacint69 <kovacs.jacint02@gmail.com>
2025-03-26 15:20:40 +0100

    Created unathorized navbar.

--- WorkoutOptimization.FrontEnd/src/app/app-routing.module.ts
+++ WorkoutOptimization.FrontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { UnathorizedNavComponent } from './unathorized-nav/unathorized-nav.component';
 
-const routes: Routes = [{ path: 'welcome', component: WelcomeComponent }];
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'unauthorizednav', component: UnathorizedNavComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
