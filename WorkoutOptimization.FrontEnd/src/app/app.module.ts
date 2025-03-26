041e39dca6c4b004c5459390a53444d7cf5d856e
Jacint69 <kovacs.jacint02@gmail.com>
2025-03-26 18:54:29 +0100

    Added httpclient to providers.

--- WorkoutOptimization.FrontEnd/src/app/app.module.ts
+++ WorkoutOptimization.FrontEnd/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { UnathorizedNavComponent } from './unathorized-nav/unathorized-nav.component';
+import { provideHttpClient } from '@angular/common/http';
 
 //material
 import { MatIconModule } from '@angular/material/icon';
@@ -13,9 +14,15 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ApplicationComponent } from './application/application.component';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent, UnathorizedNavComponent, LandingPageComponent, ApplicationComponent],
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    UnathorizedNavComponent,
+    LandingPageComponent,
+    ApplicationComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, MatIconModule, MatTabsModule],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
