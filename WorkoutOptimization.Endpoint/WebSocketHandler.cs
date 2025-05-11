1e6f432d32adea5ee4b028cf20c1ad0545d59042
Jacint69 <kovacs.jacint02@gmail.com>
2025-05-11 13:16:59 +0200

    Added start, stop message sender to connected clients.

---
+++ WorkoutOptimization.Endpoint/WebSocketHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Concurrent;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace WorkoutOptimization.Endpoint
+{
+    public static class WebSocketHandler
+    {
+        private static ConcurrentBag<WebSocket> _sockets = new();
+
+        public static void AddClient(WebSocket socket)
+        {
+            _sockets.Add(socket);
+        }
+
+        public static async Task BroadCastMessage(string message)
+        {
+            var buffer = Encoding.UTF8.GetBytes(message);
+            var segment= new ArraySegment<byte>(buffer);
+            foreach (var socket in _sockets)
+            {
+                if (socket.State == WebSocketState.Open)
+                {
+                    await socket.SendAsync(segment, WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+            }
+        }
+    }
+}
