b9201e8ed8b4f7883a3742a02abf2b6470b9b1b9
Jacint69 <kovacs.jacint02@gmail.com>
2025-05-12 10:00:40 +0200

    Fixed socket connection exception.

--- WorkoutOptimization.Endpoint/Controllers/WebsocketController.cs
+++ WorkoutOptimization.Endpoint/Controllers/WebsocketController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Net.WebSockets;
 using System.Text;
+using WorkoutOptimization.Logic;
+using WorkoutOptimization.Models;
 
 namespace WorkoutOptimization.Endpoint.Controllers
 {
@@ -8,6 +11,12 @@ namespace WorkoutOptimization.Endpoint.Controllers
     [ApiController]
     public class WebsocketController : ControllerBase
     {
+
+        private readonly IGyroscopeDataLogic _gyroscopeDataLogic;
+        public WebsocketController(IGyroscopeDataLogic gyroscopeDataLogic)
+        {
+            this._gyroscopeDataLogic = gyroscopeDataLogic;
+        }
         [HttpGet("connect")]
         public async Task<IActionResult> Connect()
         {
@@ -17,19 +26,40 @@ namespace WorkoutOptimization.Endpoint.Controllers
                 return BadRequest();
             }
 
-            var token= HttpContext.Request.Headers["Authorization"].ToString();
-            var tokenFromRequest = HttpContext.Request.Query["access_token"];
+            //var token= HttpContext.Request.Headers["Authorization"].ToString();
+            //var tokenFromRequest = HttpContext.Request.Query["access_token"];
             ; 
 
             //Hozzáadjuk a bag-hez a socketet
             var socket= await HttpContext.WebSockets.AcceptWebSocketAsync();
             WebSocketHandler.AddClient(socket);
 
-            var buffer= new byte[1024];
-            while (socket.State == WebSocketState.Open) { 
-                var res= await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                var msg = Encoding.UTF8.GetString(buffer, 0, res.Count);
-                Console.WriteLine($"[Client]: {msg}");
+            var buffer = new byte[1024 * 4];
+
+            try
+            {
+                while (socket.State == WebSocketState.Open)
+                {
+                    var res = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                    if (res.MessageType == WebSocketMessageType.Close)
+                    {
+                        await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+                        break;
+                    }
+
+                    var msg = Encoding.UTF8.GetString(buffer, 0, res.Count);
+                    GyroscopeDataDto gyD = JsonConvert.DeserializeObject<GyroscopeDataDto>(msg)!;
+                    _gyroscopeDataLogic.Create(gyD);
+                }
+            }
+            catch (WebSocketException ex)
+            {
+                Console.WriteLine($"WebSocket error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"General error: {ex.Message}");
             }
             return new EmptyResult();
 
