4b32f8879d0a53f34a076d326c39cb72eb4b5eb1
Jacint69 <kovacs.jacint02@gmail.com>
2025-06-19 10:30:36 +0200

    Added checkpoint.

--- WorkoutOptimization.Data/model.py
+++ WorkoutOptimization.Data/model.py
@@ -3,7 +3,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout, BatchNormalization
 from sklearn.preprocessing import MinMaxScaler
@@ -13,13 +13,8 @@ import joblib
 with open('normalized_save.json', 'r') as f:
     data = json.load(f)  # Lista ismétlésekből, mind 14 elem (13 adat + 1 IsCorrect)
 
-with open('validation.json', 'r') as f:
-    data_validation = json.load(f)  # Lista ismétlésekből, mind 14 elem (13 adat + 1 IsCorrect)
-
 X = []
 y = []
-X_val= []
-y_val = []
 
 for ism in data:
     # Első 13 elem az adat:
@@ -36,29 +31,6 @@ y = np.array(y)  # (mintaszám,)
 from sklearn.utils import shuffle
 X, y = shuffle(X, y, random_state=42)
 
-##Validációs
-for ism in data_validation:
-    # Első 13 elem az adat:
-    seq_np = np.array([[p['GyrosX'], p['GyrosY'], p['GyrosZ'],
-                        p['AccelX'], p['AccelY'], p['AccelZ']] for p in ism[:13]])
-    X_val.append(seq_np)
-
-    # A 14. elem a címke:
-    label = ism[13].get('IsCorrect', False)
-    y_val.append(1 if label else 0)
-
-X_val = np.array(X_val)  # (mintaszám, 13, 6)
-y_val = np.array(y_val)  # (mintaszám,)
-from sklearn.utils import shuffle
-X_val, y_val = shuffle(X_val, y_val, random_state=42)
-print(f'Validációs Adat méret: {X_val.shape}')
-print(f'Validációs Címke méret: {y_val.shape}')
-print(y_val)
-##
-
-
-
-
 print(f'Adat méret: {X.shape}')
 print(f'Címke méret: {y.shape}')
 print(y)
@@ -68,11 +40,6 @@ scaler = MinMaxScaler()
 X_reshaped = X.reshape(-1, 6)         # (mintaszám × 13, 6)
 X_scaled = scaler.fit_transform(X_reshaped)
 X_scaled = X_scaled.reshape(-1, 13, 6)
-
-# Validációs adatok
-X_val_reshaped = X_val.reshape(-1, 6)
-X_val_scaled = scaler.transform(X_val_reshaped)
-X_val_scaled = X_val_scaled.reshape(-1, 13, 6)
 
 # (Menthető fájlba is, ha később használni szeretnéd)
 joblib.dump(scaler, 'scaler.save')
@@ -101,26 +68,33 @@ early_stop = EarlyStopping(
     restore_best_weights=True
 )
 
+checkpoint = ModelCheckpoint(
+    filepath='best_model.h5',
+    monitor='val_accuracy',
+    save_best_only=True,
+    mode='max',
+    verbose=1
+)
+
 # 4) Betanítás
-history = model.fit(X_scaled, y, epochs=30, batch_size=2, validation_data=(X_val_scaled, y_val), callbacks=[early_stop])
+history = model.fit(X_scaled, y, epochs=30, batch_size=5, validation_split=0.3, callbacks=[early_stop, checkpoint])
 
 # 5) Egy új ismétlésre való predikció
 sample = [
-    [59.547, 46.42406, -105.8785, 0.728595, 0.602798, 0.0],
-    [58.59538, 39.26905, -103.3342, 0.607721, 0.714048, 0.0],
-    [50.56436, 35.35977, -84.64791, 0.531167, 0.800273, 0.0],
-    [-2.624928, 16.02285, 15.60026, 0.467554, 0.900926, 0.0],
-    [-37.22574, 17.26453, 68.12836, 0.512591, 0.900399, 0.0],
-    [-66.28752, 0.0, 108.3011, 0.606958, 0.83023, 0.11001],
-    [-93.81532, -18.80878, 138.2111, 0.703933, 0.497684, 0.244042],
-    [-93.36436, -24.23364, 132.0903, 0.77122, 0.300625, 0.244584],
-    [-83.77387, -27.22494, 115.7096, 0.8192, 0.115662, 0.232879],
-    [-13.56852, -12.91328, 17.45618, 0.815402, 0.0, 0.176147],
-    [18.4481, -1.140374, -38.36639, 0.824859, 0.0, 0.195007],
-    [34.61699, 12.82127, -68.946, 0.846932, 0.0, 0.221026],
-    [63.96709, 29.2272, -119.3012, 0.788664, 0.417514, 0.252783]
+    [105.9261, 159.8105, -150.3793, 0.764807, 0.558656, 0.0],
+    [84.5711919335899, 143.84911124081674, -148.9160464706417, 0.6956400136821047, 0.6575745315740832, 0.0],
+    [69.65711909322776, 112.10006231458102, -139.01391641511958, 0.6228802826875413, 0.7478405919515393, 0.0],
+    [46.79487596435616, 91.72893203508079, -86.34624372132333, 0.5812871172797972, 0.8244285859529148, 0.0],
+    [24.052214362416706, 61.355688669387945, -18.202957978833165, 0.5485982041347139, 0.8850784900072927, 0.0],
+    [8.103849945447955, 41.52685042750308, 35.84500796167725, 0.5394272190834439, 0.8910828380756373, 0.0],
+    [-1.3055023205188674, 29.44535447004717, 74.23750243042454, 0.5934992858490566, 0.852322121462264, 0.0],
+    [-9.315706626931611, 17.21664993968989, 98.60432828171677, 0.6601580726814933, 0.7794338614570322, 0.0],
+    [-13.63516922097715, 12.909334139976156, 95.06290935199877, 0.6727581940483748, 0.6602675113903315, 0.0],
+    [-7.701362085582571, 8.073237061735224, 55.15375238641768, 0.6875510129088822, 0.5634525278442548, 0.0],
+    [2.546164310336188, 4.87955049132394, 0.3961806219679494, 0.7405643671202855, 0.5530593309170843, 0.0],
+    [12.294896597404612, 11.350939484416934, -43.0052221969312, 0.741159647352574, 0.5725632952297255, 0.0],
+    [22.67217, 19.28003, -72.07409, 0.717876, 0.633565, 0.0]
 ]
-
 sample2 = [
     [-15.1626, 21.75574, -34.74697, 0.475283, 0.904518, 0.0],
     [-11.97223, 7.577221, -19.76566, 0.458235, 0.91932, 0.0],
@@ -138,15 +112,18 @@ sample2 = [
 ]
 # Normalizálás predikció előtt
 sample = np.array(sample).reshape(-1, 6)
+sample2 = np.array(sample2).reshape(-1, 6)
 sample_scaled = scaler.transform(sample).reshape(1, 13, 6)
+sample_scaled2 = scaler.transform(sample2).reshape(1, 13, 6)
 
 # Előrejelzés
 prediction = model.predict(sample_scaled)
+prediction2 = model.predict(sample_scaled2)
 print("Valószínűség, hogy helyes:", prediction[0][0])
 print("Helyes gyakorlat?", prediction[0][0] > 0.5)
 # 6) Validációs statisztika
 val_acc = history.history['val_accuracy'][-1]
-num_val_samples = int(len(X_val_scaled))
+num_val_samples = int(len(X_scaled) * 0.3)
 correct_predictions = int(val_acc * num_val_samples)
 
 print(f"A validációs mintákból {correct_predictions}/{num_val_samples}-t talált el.")
