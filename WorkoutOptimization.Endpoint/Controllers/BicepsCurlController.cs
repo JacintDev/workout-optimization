3aeb0d90592ecd63d277adb279e276461ecdd4cf
Jacint69 <kovacs.jacint02@gmail.com>
2025-06-20 18:04:20 +0200

    Added BicepsCurl controller.

---
+++ WorkoutOptimization.Endpoint/Controllers/BicepsCurlController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkoutOptimization.Logic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WorkoutOptimization.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    
+    public class BicepsCurlController : ControllerBase
+    {
+        readonly IBicepsCurlLogic _logic;
+
+        public BicepsCurlController(IBicepsCurlLogic logic)
+        {
+            _logic = logic;
+        }
+
+        [HttpPost]
+        public bool Get(float[][] inputData)
+        {
+            // Ha ragaszkodsz a 3D tömbhöz, konvertáld át itt pl.:
+            float[,,] converted = new float[1, 13, 6];
+            for (int j = 0; j < 13; j++)
+            {
+                for (int k = 0; k < 6; k++)
+                {
+                    converted[0, j, k] = inputData[j][k];
+                }
+            }
+            return _logic.DataValidation(converted);
+        }
+
+    }
+}
