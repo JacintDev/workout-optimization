f0da1377801f81256fdddf4c8d286b1213a704b4
Jacint69 <kovacs.jacint02@gmail.com>
2025-10-07 22:04:09 +0200

    Moved home component methods to training components

--- WorkoutOptimization.FrontEnd/src/app/services/training.service.ts
+++ WorkoutOptimization.FrontEnd/src/app/services/training.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WorkoutSessionCount } from '../../models/WorkoutSessionCount';
+import { StartTrainingModel } from '../../models/StartTrainingModel';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TrainingService {
+  private link: string = 'http://localhost:5135/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getWorkoutSessions(): Observable<WorkoutSessionCount[]> {
+    return this.http.get<WorkoutSessionCount[]>(
+      `${this.link}Training/CountWorkoutSessions`
+    );
+  }
+  startWebSocketSending(): Observable<any> {
+    return this.http.get<any>(`${this.link}Websocket/Start/`);
+  }
+  stopWebSocketSending(): Observable<any> {
+    return this.http.get<any>(`${this.link}Websocket/Stop/`);
+  }
+  startTraining(training: StartTrainingModel): Observable<any> {
+    return this.http.post<any>(`${this.link}Training/CreateTraining`, training);
+  }
+
+  getActiveTraining(): Observable<any> {
+    return this.http.get<any>(`${this.link}Training/GetActiveTraining`);
+  }
+
+  stopTraining(trainingId: number): Observable<any> {
+    return this.http.put<any>(
+      `${this.link}Training/StopTraining/${trainingId}`,
+      null
+    );
+  }
 }
--- WorkoutOptimization.FrontEnd/src/app/training/training.component.ts
+++ WorkoutOptimization.FrontEnd/src/app/training/training.component.ts
@@ -1,11 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TrainingService } from '../services/training.service';
+import { WorkoutSessionCount } from '../../models/WorkoutSessionCount';
+import { Observable } from 'rxjs';
+import { StartTrainingModel } from '../../models/StartTrainingModel';
+import * as signalR from '@microsoft/signalr';
 
 @Component({
   selector: 'app-training',
   standalone: false,
   templateUrl: './training.component.html',
-  styleUrl: './training.component.sass'
+  styleUrls: ['./training.component.sass'],
 })
-export class TrainingComponent {
+export class TrainingComponent implements OnInit, OnDestroy {
+  workoutSessions$!: Observable<WorkoutSessionCount[]>;
+  private hubConnection!: signalR.HubConnection;
+  public predictionMessage: string = '';
+  isActiveTraining: boolean = false;
+  trainingId: number = 0;
+  training: StartTrainingModel = new StartTrainingModel();
+  constructor(private trainingService: TrainingService) {}
 
+  ngOnInit(): void {
+    this.workoutSessions$ = this.trainingService.getWorkoutSessions();
+
+    //SignalR
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('http://localhost:5135/exercisehub')
+      .build();
+    this.hubConnection
+      .start()
+      .then(() => {
+        console.log('SignalR connection started');
+      })
+      .catch((err) => console.error('SignalR error:', err));
+
+    this.hubConnection.on('ReceivePrediction', (message: string) => {
+      this.predictionMessage = message;
+    });
+  }
+  ngOnDestroy(): void {
+    if (this.hubConnection) {
+      this.hubConnection.stop().then(() => console.log('SignalR Disconnected'));
+    }
+  }
+
+  startTraining() {
+    this.startWebSocketSending();
+    this.training.start = new Date().toISOString();
+    this.training.isActive = true;
+    this.training.exerciseId = 1;
+
+    this.trainingService.startTraining(this.training).subscribe({
+      next: (res) => this.getActiveTraining(),
+      error: (err) => console.log(err),
+    });
+  }
+  private startWebSocketSending() {
+    this.trainingService.startWebSocketSending().subscribe({
+      next: (res) => console.log(res),
+      error: (err) => console.log(err),
+    });
+  }
+  private getActiveTraining() {
+    this.trainingService.getActiveTraining().subscribe({
+      next: (res) => {
+        this.isActiveTraining = true;
+        this.trainingId = res.trainingId;
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  stopTraining() {
+    this.stopWebSocketSending();
+    this.trainingService.stopTraining(this.trainingId).subscribe({
+      next: (res) => {
+        this.isActiveTraining = false;
+        console.log(res);
+      },
+      error: (err) => console.log(err),
+    });
+  }
+
+  private stopWebSocketSending() {
+    this.trainingService.stopWebSocketSending().subscribe({
+      next: (res) => console.log(res),
+      error: (err) => console.log(err),
+    });
+  }
 }
