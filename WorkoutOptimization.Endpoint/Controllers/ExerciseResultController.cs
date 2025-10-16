8f1fb3b72d0ac717b463a09c65c2c9c1112332d3
Jacint69 <kovacs.jacint02@gmail.com>
2025-10-16 14:28:02 +0200

    Created ExerciseResult controller.

---
+++ WorkoutOptimization.Endpoint/Controllers/ExerciseResultController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WorkoutOptimization.Logic;
+using WorkoutOptimization.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WorkoutOptimization.Endpoint.Controllers
+{
+    [Authorize]
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class ExerciseResultController : ControllerBase
+    {
+        readonly IExerciseResultLogic _exerciseResultLogic;
+        readonly UserManager<User> _userManager;
+        public ExerciseResultController(IExerciseResultLogic exerciseResultLogic, UserManager<User> userManager)
+        {
+            _exerciseResultLogic = exerciseResultLogic;
+            _userManager = userManager;
+        }
+
+
+        // GET: api/<ExerciseResultController>
+        [HttpGet("{id}")]
+        public async Task<IEnumerable<ExerciseResultReturnedValueModel>> GetAllById(int id)
+        {
+            return await _exerciseResultLogic.ReadAllById(id);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ExerciseResultReturnedValueModel>> GetAllByUser()
+        {
+            var user = await _userManager.FindByEmailAsync(User.Identity.Name);
+            if (user == null)
+            {
+                throw new UnauthorizedAccessException("Invalid user");
+            }
+            return await _exerciseResultLogic.ReadAllByUser(user);
+        }
+
+
+
+
+    }
+}
