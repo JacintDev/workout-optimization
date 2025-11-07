43d5df2766f76b0a0f0a08ad95b80bd9ca802ab2
Jacint69 <kovacs.jacint02@gmail.com>
2025-11-07 13:49:07 +0100

    PulseController edited.

---
+++ WorkoutOptimization.Endpoint/Controllers/PulseController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WorkoutOptimization.Logic.Interfaces;
+using WorkoutOptimization.Models.Entities;
+using WorkoutOptimization.Models.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WorkoutOptimization.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class PulseController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IPulseLogic _pulseLogic;
+
+        public PulseController(UserManager<User> userManager, IPulseLogic pulseLogic)
+        {
+            _userManager = userManager;
+            _pulseLogic = pulseLogic;
+        }
+
+        // GET: api/<PulseController>
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+            
+        //}
+
+        // GET api/<PulseController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PulseController>
+        [HttpPost()]
+        public async Task<IActionResult> Post([FromBody] PulseSendModel pulse)
+        {
+            var user = await _userManager.FindByEmailAsync(User.Identity.Name);
+            var res = _pulseLogic.CompareToRestPulse(user, (int)pulse.Pulse);
+            return Ok(new
+            {
+                message = res.Item1,
+                pulse = res.Item2
+            });
+
+
+        }
+
+        // PUT api/<PulseController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<PulseController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
