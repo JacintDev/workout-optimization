7f3dcd08c99e6fe1d945c4a3b6d9b1fa3a85cf79
Jacint69 <kovacs.jacint02@gmail.com>
2025-12-07 19:04:19 +0100

    Implemented responsive layout for navbar.

--- WorkoutOptimization.FrontEnd/src/app/authorized-navbar/authorized-navbar.component.ts
+++ WorkoutOptimization.FrontEnd/src/app/authorized-navbar/authorized-navbar.component.ts
@@ -4,8 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-authorized-navbar',
   standalone: false,
   templateUrl: './authorized-navbar.component.html',
-  styleUrl: './authorized-navbar.component.sass'
+  styleUrl: './authorized-navbar.component.sass',
 })
 export class AuthorizedNavbarComponent {
+  isMenuOpen = false;
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
 }
