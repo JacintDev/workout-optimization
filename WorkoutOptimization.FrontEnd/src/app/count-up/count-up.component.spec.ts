2fe7dac80126b369fdfa5e1ae4400adb4e5a5c75
Jacint69 <kovacs.jacint02@gmail.com>
2025-12-07 19:05:35 +0100

    Created count up animation.

---
+++ WorkoutOptimization.FrontEnd/src/app/count-up/count-up.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CountUpComponent } from './count-up.component';
+
+describe('CountUpComponent', () => {
+  let component: CountUpComponent;
+  let fixture: ComponentFixture<CountUpComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CountUpComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CountUpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
