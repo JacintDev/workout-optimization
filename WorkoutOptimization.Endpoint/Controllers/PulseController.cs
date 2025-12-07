89373c52626cbcbce681908052295ce238c7b3d0
Jacint69 <kovacs.jacint02@gmail.com>
2025-12-07 19:08:43 +0100

    Ignored junks.

--- WorkoutOptimization.Endpoint/Controllers/PulseController.cs
+++ WorkoutOptimization.Endpoint/Controllers/PulseController.cs
@@ -28,19 +28,8 @@ namespace WorkoutOptimization.Endpoint.Controllers
             _hubContext = hubContext;
         }
 
-        // GET: api/<PulseController>
-        //[HttpGet]
-        //public IActionResult Get()
-        //{
 
-        //}
 
-        // GET api/<PulseController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
 
         // POST api/<PulseController>
         [HttpPost()]
@@ -51,19 +40,11 @@ namespace WorkoutOptimization.Endpoint.Controllers
             await _hubContext.Clients.All.SendAsync("ReceivePulse", res);
             return res;
 
-
         }
 
-        // PUT api/<PulseController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
 
-        // DELETE api/<PulseController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+
+
+
     }
 }
