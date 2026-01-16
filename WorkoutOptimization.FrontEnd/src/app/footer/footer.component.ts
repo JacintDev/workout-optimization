3723b57b8234d9cc255cd46357997eb046331bd1
Jacint69 <kovacs.jacint02@gmail.com>
2026-01-16 19:50:12 +0100

    Implemented footer component.

---
+++ WorkoutOptimization.FrontEnd/src/app/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: false,
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.sass',
+})
+export class FooterComponent {
+  scrollTo(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+}
