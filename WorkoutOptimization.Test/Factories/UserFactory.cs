80518839e241d4a8dac3243359b453465bcc1710
Jacint69 <kovacs.jacint02@gmail.com>
2026-01-19 18:26:32 +0100

    Generated User Registration test with bogus.

---
+++ WorkoutOptimization.Test/Factories/UserFactory.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutOptimization.Models.Models;
+
+namespace WorkoutOptimization.Test.Factories
+{
+    public static class UserFactory
+    {
+
+        private static Faker<RegisterModel> _userFaker = new Faker<RegisterModel>("en")
+            .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+            .RuleFor(u => u.LastName, f => f.Name.LastName())
+            .RuleFor(u => u.Email, (f, u) => f.Internet.Email(u.FirstName, u.LastName))
+            .RuleFor(u => u.Password, f => f.Internet.Password(8))
+            .RuleFor(u => u.Sex, f => f.PickRandom<Sex>());
+
+        public static RegisterModel CreateValidUser() {
+        
+            return _userFaker.Generate();
+        }
+    }
+}
